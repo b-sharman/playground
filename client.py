33bcc6875af2c9ee0aaebf0d3e9da36e8f220ca2
nonoesimposible <nonoesimposible72@gmail.com>
2023-06-09 06:36:41 -0600

    use aioconsole to prevent blocking keepalive pings

--- client.py
+++ client.py
@@ -2,17 +2,18 @@ import asyncio
 import logging
 import websockets
 
+import aioconsole
+
 import constants
 
 async def data_entry(ws):
     while True:
-        data = input("Enter data: ")
+        data = await aioconsole.ainput("Enter data: ")
         await ws.send(data)
 
 async def main():
     async with websockets.connect(f"ws://localhost:{constants.PORT}") as ws:
-        task = asyncio.create_task(data_entry(ws))
-        await task
+        await asyncio.create_task(data_entry(ws))
         while True:
             print(await ws.recv())
 
